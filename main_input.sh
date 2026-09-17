3ae972b595ee3c359ecae46faa9a23f212055a4f
Diego Derman <diegoderman@gmail.com>
2026-09-17 12:23:49 +0200

    Prepared for conda venv

--- main_input.sh
+++ main_input.sh
@@ -8,7 +8,7 @@ source $config_path
 
 ##### 0. First time participant database creation
 
-python3.9 $NIBS_SRC_DIR/first_database.py --config $config_path
+$PYTHON_BIN $NIBS_SRC_DIR/first_database.py --config $config_path
 
 ######################################################################
 #
